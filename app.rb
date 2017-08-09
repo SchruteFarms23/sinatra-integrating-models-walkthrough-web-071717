079ad905eda3c3392ec4c36427ae47f79bf3601a
SchruteFarms23 <romanmullady@gmail.com>
2017-08-09 11:02:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ end
 
 post '/' do
   text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+  #or you can make it even shorter =>@analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
 
